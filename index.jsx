03be06831f69fdc3e5d9e743e16c124a78be3f7a
Moha <moha@localhost.localdomain>
2019-06-12 23:47:29 +0430

    cross-origin

--- index.jsx
+++ index.jsx
@@ -3,81 +3,119 @@ const Joi = require("joi");
 const app = express();
 
 app.use(express.json());
-
-courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-  { id: 4, name: "course4" }
+products = [
+  {
+    id: 1,
+    name: "Product One",
+    description: "Product one sample description",
+    imageUrl: "https://picsum.photos/200",
+    value: 0
+  },
+  {
+    id: 2,
+    name: "Product Two",
+    description: "Product two sample description",
+    imageUrl: "https://picsum.photos/200",
+    value: 0
+  },
+  {
+    id: 3,
+    name: "Product Three",
+    description: "Product three sample description",
+    imageUrl: "https://picsum.photos/200",
+    value: 0
+  },
+  {
+    id: 4,
+    name: "Product Four",
+    description: "Product four sample description",
+    imageUrl: "https://picsum.photos/200",
+    value: 0
+  },
+  {
+    id: 5,
+    name: "Product Five",
+    description: "Product five sample description",
+    imageUrl: "https://picsum.photos/200",
+    value: 0
+  },
+  {
+    id: 6,
+    name: "Product Six",
+    description: "Product six sample description",
+    imageUrl: "https://picsum.photos/200",
+    value: 0
+  }
 ];
 
 app.get("/", (req, res) => {
-  res.send("Courses Repository");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.send("products Repository");
 });
 
 app.get("/api", (req, res) => {
   res.send("Please specify method");
 });
 
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
+app.get("/api/products", (req, res) => {
+  res.send(products);
 });
 
-app.get("/api/courses/:id", (req, res) => {
-  let course = courseExistance(req.params, res);
-  res.send(course);
+app.get("/api/products/:id", (req, res) => {
+  let product = productExistance(req.params, res);
+  res.send(product);
 });
 
-app.post("/api/courses", (req, res) => {
-  const { error } = validateCourse(req.body);
+app.post("/api/products", (req, res) => {
+  const { error } = validateProduct(req.body);
   if (error) {
     res.status(400).send(`Bad request: ${error.details[0].message}`);
     return;
   }
-  const course = {
-    id: courses.length + 1,
+  const product = {
+    id: products.length + 1,
     name: req.body.name
   };
 
-  courses.push(course);
-  res.send(course);
+  products.push(product);
+  res.send(product);
 });
 
-app.put("/api/courses/:id", (req, res) => {
-  course = courseExistance(req.params, res);
-  const { error } = validateCourse(req.body);
+app.put("/api/products/:id", (req, res) => {
+  product = productExistance(req.params, res);
+  const { error } = validateProduct(req.body);
   if (error) {
     res.status(400).send(`Bad request: ${error.details[0].message}`);
     return;
   }
 
-  course.name = req.body.name;
-  res.send(course);
-});
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  //something
+  product.name = req.body.name;
+  res.send(product);
 });
 
-app.delete("/api/courses/:id", (req, res) => {
-  let course = courseExistance(req.params, res);
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  res.send(course);
+app.delete("/api/products/:id", (req, res) => {
+  let product = productExistance(req.params, res);
+  const index = products.indexOf(product);
+  products.splice(index, 1);
+  res.send(product);
 });
 
-function validateCourse(course) {
+function validateProduct(product) {
   const schema = {
     name: Joi.string().required()
   };
 
-  return Joi.validate(course, schema);
+  return Joi.validate(product, schema);
 }
 
-function courseExistance(params, res) {
-  let course = courses.find(c => c.id === parseInt(params.id));
-  if (!course)
+function productExistance(params, res) {
+  let product = products.find(c => c.id === parseInt(params.id));
+  if (!product)
     //404
-    return res.status(404).send("Course n ot found");
-  return course;
+    return res.status(404).send("product n ot found");
+  return product;
 }
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
